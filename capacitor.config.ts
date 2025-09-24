f0e7d0abbe5de270100fc58fdf482896ba90385f
Leslie Kardashev <lesliekardashev@gmail.com>
2025-09-24 11:23:29 +0000

    demo data fix , order hierachy fix

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thonketdriver.app',
+  appName: 'ThonketDriver',
+  webDir: 'out'
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* ... other config ... */
+  output: 'export',
   images: {
     unoptimized: true
   }
