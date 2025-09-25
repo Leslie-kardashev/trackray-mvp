00123f5a87d52c955d9a5ea9c623e03a1863a1f8
Leslie Kardashev <lesliekardashev@gmail.com>
2025-09-25 19:33:39 +0000

    Reduce font size a little (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="hidden md:flex flex-col items-center justify-center p-10 text-center relative overflow-hidden">
            <div className="absolute inset-0 bg-gradient-to-br from-purple-900 to-blue-900" />
            <div className="relative z-10 mt-auto max-w-md text-white text-left">
-            <h1 className="font-headline text-4xl font-bold tracking-tight">
+            <h1 className="font-headline text-3xl font-bold tracking-tight">
                 Bleeding Edge Inventory Management System
             </h1>
             <p className="mt-2 text-white/80">
