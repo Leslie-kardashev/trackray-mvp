20efddcada0719186d27cde9cc2280c8f616dd35
Leslie Kardashev <lesliekardashev@gmail.com>
2025-09-25 03:22:30 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -10,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -206,6 +207,12 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetHeader className="p-4 border-b">
+                <SheetTitle className="text-left">Main Navigation</SheetTitle>
+                <SheetDescription className="sr-only">
+                    Use this menu to navigate between the main sections of the customer portal.
+                </SheetDescription>
+            </SheetHeader>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
@@ -761,3 +768,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
